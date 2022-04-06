be3a4e8ad6a0b9009ae373d0fb4bb36a03f8c711
bukkeyrakeshnaik <98520924+bukkeyrakeshnaik@users.noreply.github.com>
2022-04-06 13:27:26 +0530

    Delete 7.2program directory

--- 7.2program/src/advancetraining/com/NameNotValidException.java
+++
@@ -1,9 +0,0 @@
-package advancetraining.com;
-
-class NameNotValidException extends Exception
-{
-     public String validname()
-     {
-          return ("Name is not Valid..Please ReEnter the Name");
-     }
-}
