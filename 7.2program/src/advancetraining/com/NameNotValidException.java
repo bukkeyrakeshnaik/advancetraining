26c075f19bb98d4b8e5086ba241bf1bb929bfbf5
bukkeyrakeshnaik <98520924+bukkeyrakeshnaik@users.noreply.github.com>
2022-04-06 12:09:08 +0530

    Advancetraining

---
+++ 7.2program/src/advancetraining/com/NameNotValidException.java
@@ -0,0 +1,9 @@
+package advancetraining.com;
+
+class NameNotValidException extends Exception
+{
+     public String validname()
+     {
+          return ("Name is not Valid..Please ReEnter the Name");
+     }
+}
